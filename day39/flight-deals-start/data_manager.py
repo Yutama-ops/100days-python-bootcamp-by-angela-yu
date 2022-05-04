3fe30307d1eeacd41d3770c04b1c88c6320326c7
tamcrush <tamcrush@yahoo.com>
2022-05-04 14:34:17 +1000

    a

--- day39/flight-deals-start/data_manager.py
+++ day39/flight-deals-start/data_manager.py
@@ -1,29 +1,34 @@
 import requests
+
+SHEETY_TOKEN = "Bearer sdjahcgfikaswudygfc;oajs"
+SHEETY_PRICES_ENDPOINT = "https://api.sheety.co/43e1951c5c9ac91cd68adc6a3cc50dd5/flightDetails/prices"
+
 class DataManager:
+
+
     def __init__(self):
-        self.endpoint = "https://api.sheety.co/43e1951c5c9ac91cd68adc6a3cc50dd5/flightDetails/prices"
-        self.token = "Bearer sdjahcgfikaswudygfc;oajs"
+        self.destination_data = {}
 
-    def getData(self):
-        self.response = requests.get(url=self.endpoint)
-        self.data = self.response.json()
-        return self.data['prices']
+    def get_destination_data(self):
+        # 2. Use the Sheety API to GET all the data in that sheet and print it out.
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT)
+        data = response.json()
+        self.destination_data = data["prices"]
+        # 3. Try importing pretty print and printing the data out again using pprint().
+        # pprint(data)
+        return self.destination_data
 
-    def putData(self, data):
-        for param in data:
-            params = {
+    # 6. In the DataManager Class make a PUT request and use the row id  from sheet_data
+    # to update the Google Sheet with the IATA codes. (Do this using code).
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
                 "price": {
-                    "city": param["city"],
-                    "iataCode": param["iataCode"],
-                    "lowestPrice": param["lowestPrice"]
+                    "iataCode": city["iataCode"]
                 }
             }
-            self.response = requests.put(url=f"{self.endpoint}/{param['id']}", json=params)
-            print("a")
-
-    def updateData(self,sheet_data):
-        for data in sheet_data:
-            data['iataCode'] = location_type
-
-    #This class is responsible for talking to the Google Sheet.
-    pass
+            response = requests.put(
+                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
+                json=new_data
+            )
+            print(response.text)
--- day39/flight-deals-start/flight_search.py
+++ day39/flight-deals-start/flight_search.py
@@ -3,18 +3,13 @@ import requests
 TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
 TEQUILA_API_KEY = "REMDq4a4rqQNQRNSq1b4DRv4jGVLVq0f"
 
+
 class FlightSearch:
-
-    # This class is responsible for talking to the Flight Search API.
-
 
     def get_destination_code(self, city_name):
         location_endpoint = f"{TEQUILA_ENDPOINT}/locations/query"
-        headers = {"apikey": {TEQUILA_API_KEY}}
-        query = {
-            "term": city_name,
-            "location_types": "city"
-        }
+        headers = {"apikey": TEQUILA_API_KEY}
+        query = {"term": city_name, "location_types": "city"}
         response = requests.get(url=location_endpoint, headers=headers, params=query)
         results = response.json()["locations"]
         code = results[0]["code"]
